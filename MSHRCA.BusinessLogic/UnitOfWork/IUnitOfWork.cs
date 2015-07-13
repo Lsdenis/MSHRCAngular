aa93e6c983e1a80ad156f5e84994c6883e84377e
Denis Perevalov <lsden1995@gmail.com>
2015-07-13 12:49:09 +0300

    Configure project

---
+++ MSHRCA.BusinessLogic/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using notimeReports.BusinessLogic.DataModel.Database;
+
+namespace notimeReports.BusinessLogic.UnitOfWork
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		ONT_5Entities Context { get; }
+		void Commit();
+	}
+}
---
+++ MSHRCA.BusinessLogic/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using notimeReports.BusinessLogic.DataModel.Database;
+using notimeReports.BusinessLogic.Interfaces;
+
+namespace notimeReports.BusinessLogic.UnitOfWork
+{
+	public class UnitOfWork : IUnitOfWork, IDependency
+	{
+		private readonly ONT_5Entities _context;
+
+		public UnitOfWork(ONT_5Entities context)
+		{
+			_context = context;
+		}
+
+		public ONT_5Entities Context
+		{
+			get { return _context; }
+		}
+
+		public void Commit()
+		{
+			_context.SaveChanges();
+		}
+
+		public void Dispose()
+		{
+			if (_context != null)
+			{
+				_context.Dispose();
+			}
+		}
+	}
+}
